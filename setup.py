23032b678dba966fa32909462093dc18845eebc2
Peter Weidenkaff <weidenka@uni-mainz.de>
2019-10-10 09:39:54 +0200

    TravisCI: Add deployment stage
    * Upload package to pypi for each tag
    * The package version must be adjusted manually in setup.py
      before uploading the tag
    
    Pipenv:
    * Add ipykernel
    * Add shortcut to install jupyter kernel
      `pipenv run installKernel`

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def readme():
 
 
 setup(name='pycompwa',
-      version='0.0.2',
+      version='0.0.11',
       author='The ComPWA team',
       maintainer="Peter Weidenkaff",
       maintainer_email="weidenka@uni-mainz.de",
