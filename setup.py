3cddab093d3fb95d9e707f7dfdf17d2dc6f1d5a4
Remco de Boer <remco@ihep.ac.cn>
2020-02-04 12:01:22 +0100

    Fix: Add conditional setup.py cmake requirement

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+from packaging.version import LegacyVersion
+from skbuild.cmaker import get_cmake_version
+from skbuild.exceptions import SKBuildError
 import sys
 from setuptools import find_packages
 
@@ -17,6 +20,15 @@ def readme():
         return f.read()
 
 
+# Add CMake as a build requirement if cmake is not installed or is too low a version
+CMAKE_MINIMUM = "3.4"
+SETUP_REQUIRES = []
+try:
+    if LegacyVersion(get_cmake_version()) < LegacyVersion(CMAKE_MINIMUM):
+        SETUP_REQUIRES.append('cmake')
+except SKBuildError:
+    SETUP_REQUIRES.append('cmake')
+
 setup(name='pycompwa',
       version='0.1.00',
       author='The ComPWA team',
@@ -27,11 +39,11 @@ setup(name='pycompwa',
       long_description=readme(),
       license="GPLv3 or later",
       #  cmake_args=['-DSOME_FEATURE:BOOL=OFF'],
-      cmake_minimum_required_version="3.4",
+      cmake_minimum_required_version=CMAKE_MINIMUM,
       packages=find_packages(),
       data_files=[('pycompwa/', ['./ComPWA/Physics/particle_list.xml'])],
       zip_safe=False,
-      setup_requires=['cmake'],
+      setup_requires=SETUP_REQUIRES,
       tests_require=['pytest'],
       install_requires=['wheel', 'numpy>=1.14.5', 'progress>1.3',
                         'xmltodict>=0.11.0', 'matplotlib>=2.2.2',
