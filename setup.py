4bce33bb6dd6597c22732a97f466c49460853c74
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2021-10-21 10:22:53 +0200

    build: use setup.cfg and pyproject.toml
    * build: install scikit-build through pyproject.toml
    * build: extract info to setup.cfg from setup.py where possibly
    * ci: test pip installation
    * docs: convert README to Markdown
    * style: split pylint rules in pylintrc

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import sys
 
 from packaging.version import LegacyVersion
-from setuptools import find_packages
 from skbuild.cmaker import get_cmake_version
 from skbuild.exceptions import SKBuildError
 
@@ -25,68 +24,10 @@ try:
 except SKBuildError:
     SETUP_REQUIRES.append("cmake")
 
-
-with open("README.md") as stream:
-    README = stream.read()
-
 setup(
-    name="pycompwa",
-    version="0.1a6",
-    author="Common Partial Wave Analysis",
-    author_email="compwa-admin@ep1.rub.de",
-    maintainer_email="compwa-admin@ep1.rub.de",
-    url="https://github.com/ComPWA/pycompwa",
-    project_urls={
-        "Tracker": "https://github.com/ComPWA/pycompwa/issues",
-        "Changelog": "https://github.com/ComPWA/pycompwa/releases",
-        "Documentation": "https://pycompwa.rtfd.io",
-        "Source": "https://github.com/ComPWA/pycompwa",
-    },
-    description="ComPWA: The Common Partial Wave Analysis framework",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    license="GPLv3 or later",
-    keywords=[
-        "HEP",
-        "PWA",
-        "amplitude analysis",
-        "partial wave analysis",
-        "particle physics",
-        "particles",
-        "physics",
-    ],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Physics",
-    ],
     cmake_args=[
         "-DUSE_GENEVA:BOOL=OFF",
     ],
     cmake_minimum_required_version=CMAKE_MINIMUM,
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    package_data={"pycompwa": ["particle_list.xml"]},
-    zip_safe=False,
     setup_requires=SETUP_REQUIRES,
-    tests_require=["pytest"],
-    python_requires=">=3.6",
-    install_requires=[
-        "wheel",
-        "pandas>=1.0.0",
-        "matplotlib>=2.2.2",
-        "numpy>=1.14.5",
-        "progress>1.3",
-        "uproot3",
-        "xmltodict>=0.11.0",
-    ],
 )
